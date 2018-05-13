7f489c2d7db57d65f8fc292721e4bc9e124cf3f9
DESKTOP-R063CU7\MH <julsnutini@naver.com>
2018-05-13 11:15:43 +0900

    Retrieving the datas from the DB of cloud server. Myrecords for showing workout record is almost done, myrecords by date, ranking system is not yet

---
+++ Testing2/js/womyrec.js
@@ -0,0 +1,31 @@
+$.showUser = function(workout){
+    var obj;
+    var i;
+    $.ajax({
+        type: 'post',
+        dataType: 'json',
+        url: 'http://115.68.232.116/records/womyrec.php', //doDumbbells DB에 접근해서 ID에 맞는 정보 검색
+        data:{
+            workout : workout
+        },
+        success : function(result) {
+            if(result.success == false) {
+                alert(result.msg);
+                return;
+            }else{
+                i=0;
+                var tbl = "";
+                tbl += '<table border=1><tr><th>date</th><th>sets</th><th>reps</th><th>weight</th></tr>'
+                while(result[i]){
+                    tbl += '<tr><td>'+result[i].date+'</td><td>'+result[i].sets+'</td><td>'+result[i].reps+'</td><td>'+result[i].weight+'</td></tr>';
+                    i++;
+                }
+                tbl += '</table>'
+                document.getElementById("txtHint").innerHTML = tbl;
+            }   
+        },
+        error : function(xhr,status,erre){
+            alert(error);
+        }        
+    })
+}
