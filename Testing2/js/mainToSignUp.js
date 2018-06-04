d3c8f009300076c03a14c3392c714f47ce1f0d08
kimsungmo <tjdah9921@naver.com>
2018-06-05 00:20:36 +0900

    fff

--- Testing2/js/mainToSignUp.js
+++ Testing2/js/mainToSignUp.js
@@ -1,6 +1,10 @@
 $(function(){
 	var btn = $('#signUp');
 	btn.click(function(){
+<<<<<<< HEAD
 		tau.changePage("signUpForm.html");
+=======
+		tau.changePage("signup/signUpForm.html");
+>>>>>>> parent of 7f489c2... Retrieving the datas from the DB of cloud server. Myrecords for showing workout record is almost done, myrecords by date, ranking system is not yet
 	});
 });
